59f6a50a5d42f62c04abc9299cf6427d47fd5f9a
Samwel Kisotu <kisotusamuel2@gmail.com>
2024-09-28 13:24:12 +0300

    styles

---
+++ register.php
@@ -0,0 +1,39 @@
+
+<div class = "main-container">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Register</h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="mb-3">
+                            <label for="">First Name</label>
+                            <input type="text" name="fname" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label for="">Email Address</label>
+                            <input type="email" name="email" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label for="">User Name</label>
+                            <input type="text" name="user_name" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label for="">Password</label>
+                            <input type="password" name="password" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label for="">Confirm Password</label>
+                            <input type="text" name="confirm_password" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <button type="submit" name="register_btn" class="btn btn">Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

ea7f0047a327e88ead82bb92d4494f33f7ba2e33
Samwel Kisotu <kisotusamuel2@gmail.com>
2024-10-03 10:23:34 +0300

    check if user exist

--- controllers/RegisterController.php
+++ controllers/RegisterController.php
@@ -7,7 +7,28 @@ class RegisterController{
     }
     public function registration($fname,$email,$username,$password){
         $register_query = "INSERT INTO users(fullname,email,username,password,updated) VALUES ('$fname','$email','$username','$password') ";
+        $result = $this->conn->query($register_query);
+        return $result;
 
+    }
+    public function isUserExists($email){
+        $checkUser = "SELECT email from users WHERE email= '$email' LIMIT 1";
+        $result = $this->conn->query ($checkUser);
+        if($result -> num_rows >0){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+    public function confirmPassword($password,$confirm_password){
+        if($password==$confirm_password){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
 }

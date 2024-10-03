177a460b3a2836bbf3f6ebe11b66995f169c1e47
Samwel Kisotu <kisotusamuel2@gmail.com>
2024-10-03 10:13:25 +0300

    data insertion

---
+++ controllers/RegisterController.php
@@ -0,0 +1,16 @@
+<?php 
+include('app.php');
+class RegisterController{
+    public function __construct() {
+        $db = new DatabaseConnection;
+        $this ->conn = $db->conn;
+    }
+    public function registration($fname,$email,$username,$password,$confirm_password){
+        $register_query = "INSERT INTO users(fullname,email,username,password,updated) VALUES ('$fname','$email','$username','$password','$confirm_password') ";
+
+    }
+
+}
+
+
+?>

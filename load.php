7ab7a9457f5e0ef83d55bdec66b6cc6209399331
Samwel Kisotu <kisotusamuel2@gmail.com>
2024-10-07 09:48:15 +0300

    loading

---
+++ load.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+require "includes/constants.php";
+require "includes/dbConnection.php";
+require "lang/en.php";
+
+// Class Auto Load
+function ClassAutoload($ClassName){
+   $directories = ["forms", "processes", "structure", "tables", "global", "store"];
+
+   foreach($directories AS $dir){
+        $FileName = dirname(__FILE__) . DIRECTORY_SEPARATOR . $dir .  DIRECTORY_SEPARATOR . $ClassName . '.php';
+        
+        if(file_exists($FileName) AND is_readable($FileName)){
+         require $FileName;
+        }
+   }
+}
+spl_autoload_register('ClassAutoload');
+
+$ObjGlob = new fncs();
+$ObjSendMail = new SendMail();
+
+// Creating instances of all classes
+    $ObjLayouts = new layouts();
+    $ObjMenus = new menus();
+    $ObjContents = new contents();
+    $Objforms = new forms();
+    $conn = new dbConnection(DBTYPE, HOSTNAME, DBPORT, HOSTUSER, HOSTPASS, DBNAME);
+
+// Create process instances
+
+$ObjAuth = new auth();
+$ObjAuth->signup($conn, $ObjGlob, $ObjSendMail, $lang, $conf);

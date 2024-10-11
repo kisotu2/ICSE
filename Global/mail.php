bfa20fb45df452d64da7a694c643f2ea5e8935f6
Samwel Kisotu <kisotusamuel2@gmail.com>
2024-10-11 08:43:46 +0300

    mailer email

--- Global/mail.php
+++ Global/mail.php
@@ -22,13 +22,13 @@ public function SendMail($mailMsg){
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'musakasamuel@gmail.com';                     //SMTP username
-        $mail->Password   = '';                               //SMTP password
+        $mail->Username   = 'kisotusamuel1@gmail.com';                     //SMTP username
+        $mail->Password   = 'grcfyjpflbfcqpsd';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set 
     
         //Recipients
-        $mail->setFrom('ics@gmail.com', 'ICS 2024');
+        $mail->setFrom('kisotusamuel1@gmail.com', 'ICS 2024');
         $mail->addAddress($mailMsg['to_email'], $mailMsg['to_name']);     //Add a recipient
     
         //Content

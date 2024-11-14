934cde5a8f9ccad79f6312325222530f1f805202
Samwel Kisotu <kisotusamuel2@gmail.com>
2024-11-14 10:59:42 +0300

    register and login buttons

--- lrn/routes/web.php
+++ lrn/routes/web.php
@@ -19,7 +19,6 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-Route::get('/dbconn',function(){
-    return view('dbconn');
+Auth::routes();
 
-});
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
